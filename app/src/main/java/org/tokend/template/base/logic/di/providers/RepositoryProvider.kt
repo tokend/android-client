b39529fc319bed32b1fefb05a7a9ed996e7cfb73
Oleg Koretsky <test.radiokot@gmail.com>
2018-06-22 13:38:11 +0300

    Added favorites management

--- app/src/main/java/org/tokend/template/base/logic/di/providers/RepositoryProvider.kt
+++ app/src/main/java/org/tokend/template/base/logic/di/providers/RepositoryProvider.kt
@@ -6,11 +6,12 @@ import org.tokend.template.base.logic.repository.SystemInfoRepository
 import org.tokend.template.base.logic.repository.UserRepository
 import org.tokend.template.base.logic.repository.assets.AssetsRepository
 import org.tokend.template.base.logic.repository.balances.BalancesRepository
+import org.tokend.template.base.logic.repository.favorites.FavoritesRepository
+import org.tokend.template.base.logic.repository.pairs.AssetPairsRepository
 import org.tokend.template.base.logic.repository.tfa.TfaBackendsRepository
 import org.tokend.template.base.logic.repository.transactions.TxRepository
 import org.tokend.template.features.trade.repository.offers.OffersRepository
 import org.tokend.template.features.trade.repository.order_book.OrderBookRepository
-import org.tokend.template.base.logic.repository.pairs.AssetPairsRepository
 
 interface RepositoryProvider {
     fun balances(): BalancesRepository
@@ -24,4 +25,5 @@ interface RepositoryProvider {
     fun offers(): OffersRepository
     fun account(): AccountRepository
     fun user(): UserRepository
+    fun favorites(): FavoritesRepository
 }
