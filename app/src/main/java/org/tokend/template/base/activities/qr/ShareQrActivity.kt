a327ed817951173240bcda43d0bbe925cf3390ff
Oleg Koretsky <test.radiokot@gmail.com>
2018-08-28 13:08:38 +0300

    Added QR code image sharing

--- app/src/main/java/org/tokend/template/base/activities/qr/ShareQrActivity.kt
+++ app/src/main/java/org/tokend/template/base/activities/qr/ShareQrActivity.kt
@@ -1,8 +1,13 @@
 package org.tokend.template.base.activities.qr
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Point
+import android.net.Uri
 import android.os.Bundle
+import android.support.v4.content.FileProvider
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -14,10 +19,14 @@ import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.android.synthetic.main.activity_share_qr.*
 import org.jetbrains.anko.dimen
+import org.jetbrains.anko.doAsync
 import org.tokend.template.R
 import org.tokend.template.base.activities.BaseActivity
 import org.tokend.template.extensions.getStringExtra
 import org.tokend.template.util.ObservableTransformers
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 class ShareQrActivity : BaseActivity() {
@@ -42,6 +51,8 @@ class ShareQrActivity : BaseActivity() {
     private val topText: String
         get() = intent.getStringExtra(TOP_TEXT_EXTRA, "")
 
+    private var savedQrUri: Uri? = null
+
     override fun onCreateAllowed(savedInstanceState: Bundle?) {
         setContentView(R.layout.activity_share_qr)
         setTitle(title)
@@ -62,10 +73,19 @@ class ShareQrActivity : BaseActivity() {
 
     private fun shareData() {
         val sharingIntent = Intent(Intent.ACTION_SEND)
-        sharingIntent.type = "text/plain"
-        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
-                getString(R.string.app_name))
-        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareText)
+                .apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    type = "text/plain"
+                    putExtra(android.content.Intent.EXTRA_SUBJECT,
+                            getString(R.string.app_name))
+                    putExtra(Intent.EXTRA_TEXT, shareText)
+                }
+
+        savedQrUri?.also {
+            sharingIntent.putExtra(Intent.EXTRA_STREAM, savedQrUri)
+            sharingIntent.type = "image/png"
+        }
+
         startActivity(Intent.createChooser(sharingIntent, shareDialogText))
     }
 
@@ -102,6 +122,7 @@ class ShareQrActivity : BaseActivity() {
                             qr_code_image_view.setImageBitmap(it)
                             qr_code_image_view.visibility = View.VISIBLE
                             animateQrCode()
+                            saveQrCode(it)
                         },
                         onError = {
                             Toast.makeText(this,
@@ -121,6 +142,38 @@ class ShareQrActivity : BaseActivity() {
 
         qr_code_image_view.clearAnimation()
         qr_code_image_view.startAnimation(alphaAnimation)
+    }
+
+    private fun saveQrCode(bitmap: Bitmap) {
+        doAsync {
+            val imagesFolder = File(cacheDir, "shared")
+            try {
+                val borderSize = resources.getDimensionPixelSize(R.dimen.standard_padding)
+                val borderedBitmap = Bitmap.createBitmap(
+                        bitmap.width + borderSize * 2,
+                        bitmap.height + borderSize * 2,
+                        bitmap.config
+                )
+
+                val canvas = Canvas(borderedBitmap)
+                canvas.drawColor(Color.WHITE)
+                canvas.drawBitmap(bitmap, borderSize.toFloat(), borderSize.toFloat(), null)
+
+                imagesFolder.mkdirs()
+                val file = File(imagesFolder, "shared_qr.png")
+
+                FileOutputStream(file).use {
+                    borderedBitmap.compress(Bitmap.CompressFormat.PNG, 1, it)
+                    it.flush()
+                }
+
+                savedQrUri = FileProvider.getUriForFile(this@ShareQrActivity,
+                        "$packageName.fileprovider", file)
+
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
