4ee940564573294ca2cfc29b39dc1931692bdbdc
Oleg Koretsky <test.radiokot@gmail.com>
2018-12-04 13:29:03 +0200

    Docomented formatters

--- app/src/main/java/org/tokend/template/view/util/formatter/AmountFormatter.kt
+++ app/src/main/java/org/tokend/template/view/util/formatter/AmountFormatter.kt
@@ -12,6 +12,14 @@ object AmountFormatter {
     const val ASSET_DECIMAL_DIGITS = 6
     const val FIAT_DECIMAL_DIGITS = 2
 
+    /**
+     * Formats amount of given asset
+     *
+     * @param amount amount to format, if set to null then 0 will be used
+     * @param asset asset of the amount
+     * @param minDecimalDigits minimal count of decimal digits to display
+     * @param abbreviation to use or not to use big amount abbreviation i.e. 1100 -> 1.1K
+     */
     fun formatAssetAmount(amount: BigDecimal?,
                           asset: String? = "",
                           minDecimalDigits: Int = 0,
@@ -26,6 +34,11 @@ object AmountFormatter {
         }
     }
 
+    /**
+     * Formats amount of given asset
+     *
+     * @see formatAssetAmount
+     */
     fun formatAssetAmount(amount: String?,
                           asset: String? = "",
                           minDecimalDigits: Int = 0,
@@ -34,6 +47,13 @@ object AmountFormatter {
         return formatAssetAmount(BigDecimal(amount), asset, minDecimalDigits, abbreviation)
     }
 
+    /**
+     * Formats given amount
+     *
+     * @param amount amount to format, if set to null then 0 will be used
+     * @param maxDecimalDigits maximal count of decimal digits to display
+     * @param minDecimalDigits minimal count of decimal digits to display
+     */
     fun formatAmount(amount: BigDecimal?, maxDecimalDigits: Int,
                      minDecimalDigits: Int = 0): String {
         val amount = BigDecimalUtil.stripTrailingZeros((amount ?: BigDecimal.ZERO)
@@ -58,6 +78,9 @@ object AmountFormatter {
         }
     }
 
+    /**
+     * @return maximal decimal digits count for the asset
+     */
     fun getDecimalDigitsCount(asset: String?): Int {
         return when (asset) {
             "USD", "EUR" -> FIAT_DECIMAL_DIGITS
--- app/src/main/java/org/tokend/template/view/util/formatter/DateFormatter.kt
+++ app/src/main/java/org/tokend/template/view/util/formatter/DateFormatter.kt
@@ -5,21 +5,40 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class DateFormatter(private val context: Context) {
+    /**
+     * Formats given date to the long string:
+     * full month name, full year number, 12-/24-hour time based on device preference
+     */
     fun formatLong(date: Date): String {
-        val timeFormat = android.text.format.DateFormat.getTimeFormat(context)
-        val formattedTime = timeFormat.format(date)
-        val formattedDate = formatDateOnly(date)
-
-        return "$formattedDate $formattedTime"
+        return "${formatDateOnly(date)} ${formatTimeOnly(date)}"
     }
 
+    /**
+     * Formats given date to the compact string:
+     * short month name, 2-digits year number, 12-/24-hour time based on device preference
+     */
     fun formatCompact(date: Date): String {
-        return SimpleDateFormat("HH:mm dd MMM yy", Locale.ENGLISH)
-                .format(date)
+        val dateFormat = SimpleDateFormat("dd MMM yy", Locale.ENGLISH)
+        val formattedDate = dateFormat.format(date)
+
+        return "$formattedDate ${formatTimeOnly(date)}"
     }
 
+    /**
+     * Formats given date to the long string without time:
+     * full month name, full year number
+     */
     fun formatDateOnly(date: Date): String {
         return SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH)
                 .format(date)
+    }
+
+    /**
+     * Formats given date to the long string with time only:
+     * 12-/24-hour time based on device preference
+     */
+    fun formatTimeOnly(date: Date): String {
+        val timeFormat = android.text.format.DateFormat.getTimeFormat(context)
+        return timeFormat.format(date)
     }
 }
