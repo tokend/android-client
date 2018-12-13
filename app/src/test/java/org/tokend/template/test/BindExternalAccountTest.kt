6d1909349c04f6988dea58e6771e03600cce3ad0
Oleg Koretsky <test.radiokot@gmail.com>
2018-12-13 17:16:32 +0200

    Completed external account binding test

--- app/src/test/java/org/tokend/template/test/BindExternalAccountTest.kt
+++ app/src/test/java/org/tokend/template/test/BindExternalAccountTest.kt
@@ -2,13 +2,17 @@ package org.tokend.template.test
 
 import junit.framework.Assert
 import org.junit.Test
-import org.tokend.template.di.providers.AccountProviderFactory
-import org.tokend.template.di.providers.ApiProviderFactory
-import org.tokend.template.di.providers.RepositoryProviderImpl
-import org.tokend.template.di.providers.WalletInfoProviderFactory
+import org.tokend.template.di.providers.*
+import org.tokend.template.extensions.Asset
 import org.tokend.template.features.deposit.BindExternalAccountUseCase
 import org.tokend.template.logic.Session
 import org.tokend.template.logic.transactions.TxManager
+import org.tokend.wallet.Account
+import org.tokend.wallet.TransactionBuilder
+import org.tokend.wallet.xdr.CreateExternalSystemAccountIdPoolEntryActionInput
+import org.tokend.wallet.xdr.ManageExternalSystemAccountIdPoolEntryAction
+import org.tokend.wallet.xdr.ManageExternalSystemAccountIdPoolEntryOp
+import org.tokend.wallet.xdr.Operation
 
 class BindExternalAccountTest {
     @Test
@@ -40,6 +44,8 @@ class BindExternalAccountTest {
         Assert.assertNotNull("Environment has no assets backed by external system", asset)
         asset!!
 
+        addNewExternalSystemAddress(asset, repositoryProvider, TxManager(apiProvider))
+
         val useCase = BindExternalAccountUseCase(
                 asset.code,
                 asset.details.externalSystemType!!,
@@ -63,5 +69,39 @@ class BindExternalAccountTest {
         }
 
         Assert.assertNotNull(externalAccount)
+    }
+
+    private fun addNewExternalSystemAddress(asset: Asset,
+                                            repositoryProvider: RepositoryProvider,
+                                            txManager: TxManager) {
+        val netParams = repositoryProvider.systemInfo().getNetworkParams().blockingGet()
+
+        val action = ManageExternalSystemAccountIdPoolEntryAction.CREATE
+        val op = ManageExternalSystemAccountIdPoolEntryOp(
+                ManageExternalSystemAccountIdPoolEntryOp
+                        .ManageExternalSystemAccountIdPoolEntryOpActionInput.Create(
+                        CreateExternalSystemAccountIdPoolEntryActionInput(
+                                asset.details.externalSystemType!!,
+                                "testaddr${System.currentTimeMillis()}",
+                                0,
+                                CreateExternalSystemAccountIdPoolEntryActionInput
+                                        .CreateExternalSystemAccountIdPoolEntryActionInputExt
+                                        .EmptyVersion()
+                        )
+                ),
+                ManageExternalSystemAccountIdPoolEntryOp
+                        .ManageExternalSystemAccountIdPoolEntryOpExt
+                        .EmptyVersion()
+        )
+
+        val sourceAccount = Account.fromSecretSeed(Config.ADMIN_SEED)
+
+        val tx = TransactionBuilder(netParams, sourceAccount.accountId)
+                .addOperation(Operation.OperationBody.ManageExternalSystemAccountIdPoolEntry(op))
+                .build()
+
+        tx.addSignature(sourceAccount)
+
+        txManager.submit(tx).blockingGet()
     }
 }
