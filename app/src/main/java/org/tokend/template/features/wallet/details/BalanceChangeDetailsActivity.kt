68134d4bfbd3eb5c423a1337c638559383b36804
Oleg Koretsky <test.radiokot@gmail.com>
2019-04-10 13:49:57 +0300

    Removed unused methods from BalanceChangeDetailsActivity

--- app/src/main/java/org/tokend/template/features/wallet/details/BalanceChangeDetailsActivity.kt
+++ app/src/main/java/org/tokend/template/features/wallet/details/BalanceChangeDetailsActivity.kt
@@ -3,18 +3,14 @@ package org.tokend.template.features.wallet.details
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.util.Log
-import android.view.ViewGroup
 import org.tokend.template.R
 import org.tokend.template.activities.BaseActivity
 import org.tokend.template.data.model.history.BalanceChange
-import org.tokend.template.data.model.history.BalanceChangeAction
 import org.tokend.template.features.wallet.view.BalanceChangeIconFactory
-import org.tokend.template.view.InfoCard
 import org.tokend.template.view.details.DetailsItem
 import org.tokend.template.view.details.adapter.DetailsItemsAdapter
 import org.tokend.template.view.util.LocalizedName
 import org.tokend.template.view.util.formatter.DateFormatter
-import java.math.BigDecimal
 
 abstract class BalanceChangeDetailsActivity : BaseActivity() {
     override fun onCreateAllowed(savedInstanceState: Bundle?) {
@@ -35,13 +31,6 @@ abstract class BalanceChangeDetailsActivity : BaseActivity() {
     protected abstract fun displayDetails(item: BalanceChange)
 
     protected open fun displayDate(item: BalanceChange,
-                                   cardsLayout: ViewGroup) {
-        InfoCard(cardsLayout)
-                .setHeading(R.string.date, null)
-                .addRow(DateFormatter(this).formatLong(item.date), null)
-    }
-
-    protected open fun displayDate(item: BalanceChange,
                                    adapter: DetailsItemsAdapter) {
         adapter.addData(
                 DetailsItem(
@@ -50,13 +39,6 @@ abstract class BalanceChangeDetailsActivity : BaseActivity() {
                         icon = ContextCompat.getDrawable(this, R.drawable.ic_date)
                 )
         )
-    }
-
-    protected open fun displayEffect(item: BalanceChange,
-                                     cardsLayout: ViewGroup) {
-        InfoCard(cardsLayout)
-                .setHeading(R.string.tx_effect, null)
-                .addRow(LocalizedName(this).forBalanceChangeAction(item.action), null)
     }
 
     protected open fun displayEffect(item: BalanceChange,
@@ -70,50 +52,6 @@ abstract class BalanceChangeDetailsActivity : BaseActivity() {
                         icon = iconFactory.get(item)
                 )
         )
-    }
-
-    protected open fun displayBalanceChange(item: BalanceChange,
-                                            cardsLayout: ViewGroup) {
-        val minDigits = amountFormatter.getDecimalDigitsCount(item.assetCode)
-        val asset = item.assetCode
-
-        val titleRes: Int
-        val total: BigDecimal
-
-        val amountString = amountFormatter.formatAssetAmount(item.amount, asset, minDigits)
-        var fixedFeeString = amountFormatter.formatAssetAmount(item.fee.fixed, asset, minDigits)
-        var percentFeeString = amountFormatter.formatAssetAmount(item.fee.percent, asset, minDigits)
-
-        if (item.isReceived != null) {
-            titleRes = if (item.isReceived) {
-                R.string.received
-            } else {
-                R.string.charged
-            }
-
-            // Unlocked action is 'received' but fees are received as well as the amount.
-            if (item.isReceived && item.action != BalanceChangeAction.UNLOCKED) {
-                total = item.amount - item.fee.total
-                fixedFeeString = "-$fixedFeeString"
-                percentFeeString = "-$percentFeeString"
-            } else {
-                total = item.amount + item.fee.total
-                fixedFeeString = "+$fixedFeeString"
-                percentFeeString = "+$percentFeeString"
-            }
-
-            InfoCard(cardsLayout)
-                    .setHeading(titleRes,
-                            amountFormatter.formatAssetAmount(total, item.assetCode, minDigits))
-                    .addRow(R.string.amount, "+$amountString")
-                    .addRow(R.string.fixed_fee, fixedFeeString)
-                    .addRow(R.string.percent_fee, percentFeeString)
-        } else {
-            InfoCard(cardsLayout)
-                    .setHeading(R.string.amount, amountString)
-                    .addRow(R.string.fixed_fee, fixedFeeString)
-                    .addRow(R.string.percent_fee, percentFeeString)
-        }
     }
 
     protected open fun displayBalanceChange(item: BalanceChange,
