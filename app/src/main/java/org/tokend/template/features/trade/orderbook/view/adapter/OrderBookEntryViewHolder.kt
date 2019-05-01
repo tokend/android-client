37df198acda0c1bb509c039528197d927dde55db
Aleksandr <al.ivanchukov@gmail.com>
2019-05-01 13:34:24 +0300

    Add volume indicators in order book

--- app/src/main/java/org/tokend/template/features/trade/orderbook/view/adapter/OrderBookEntryViewHolder.kt
+++ app/src/main/java/org/tokend/template/features/trade/orderbook/view/adapter/OrderBookEntryViewHolder.kt
@@ -5,11 +5,16 @@ import android.view.View
 import android.widget.TextView
 import org.jetbrains.anko.find
 import org.jetbrains.anko.textColor
+import org.tokend.sdk.utils.BigDecimalUtil
 import org.tokend.template.R
 import org.tokend.template.features.offers.model.OfferRecord
 import org.tokend.template.features.trade.orderbook.model.OrderBookEntryRecord
 import org.tokend.template.view.adapter.base.BaseViewHolder
+import org.tokend.template.view.adapter.base.SimpleItemClickListener
 import org.tokend.template.view.util.formatter.AmountFormatter
+import java.math.BigDecimal
+import java.math.RoundingMode
+import kotlin.math.roundToInt
 
 class OrderBookEntryViewHolder(view: View,
                                private val amountFormatter: AmountFormatter
@@ -19,6 +24,20 @@ class OrderBookEntryViewHolder(view: View,
 
     private var isBuy = false
 
+    fun bind(item: OrderBookEntryListItem, clickListener: SimpleItemClickListener<OrderBookEntryListItem>?, maxVolume: BigDecimal) {
+        super.bind(item, clickListener)
+
+        val maxValue = BigDecimalUtil.scaleAmount(maxVolume,
+                0).toInt()
+
+        val itemValue = BigDecimalUtil.scaleAmount(item.volume,
+                0).toInt()
+
+        val factor = (itemValue * 100f / maxValue).roundToInt()
+
+        volumeTextView.background.level = 100 * factor
+    }
+
     override fun bind(item: OrderBookEntryListItem) {
         isBuy = item.isBuy
 
@@ -27,6 +46,7 @@ class OrderBookEntryViewHolder(view: View,
         priceTextView.text = amountFormatter.formatAssetAmount(item.price, item.quoteAssetCode,
                 amountFormatter.getDecimalDigitsCount(item.quoteAssetCode),
                 withAssetCode = false)
+
         if (isBuy) {
             priceTextView.textColor =
                     ContextCompat.getColor(view.context, R.color.received)
