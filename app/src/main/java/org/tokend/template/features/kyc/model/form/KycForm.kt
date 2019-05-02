d2223370ed970f03ccdafdf3b344a5d9626ddaf8
Oleg Koretsky <test.radiokot@gmail.com>
2019-05-02 15:50:13 +0300

    Made documents field optional in KYC form

--- app/src/main/java/org/tokend/template/features/kyc/model/form/KycForm.kt
+++ app/src/main/java/org/tokend/template/features/kyc/model/form/KycForm.kt
@@ -9,18 +9,18 @@ import org.tokend.sdk.api.documents.model.DocumentType
  */
 abstract class KycForm(
         @SerializedName("documents")
-        val documents: MutableMap<String, RemoteFile> = mutableMapOf()
+        val documents: MutableMap<String, RemoteFile>? = mutableMapOf()
 ) {
     open fun getDocument(type: DocumentType): RemoteFile? {
-        return documents[type.name.toLowerCase()]
+        return documents?.get(type.name.toLowerCase())
     }
 
     open fun setDocument(type: DocumentType, file: RemoteFile?) {
         if (file == null) {
-            documents.remove(type.name.toLowerCase())
+            documents?.remove(type.name.toLowerCase())
             return
         }
 
-        documents[type.name.toLowerCase()] = file
+        documents?.put(type.name.toLowerCase(), file)
     }
 }
