192e65d88980a2a3e90af95421e47d9a20e8714e
Oleg Koretsky <test.radiokot@gmail.com>
2019-05-10 17:16:00 +0300

    Copy recovery seed before account creation (#39)
    * Added email check before wallet creation to sign up use case
    
    * Added recovery seed confirmation to sign up use case
    
    * Placed recovery seed confirmation before account creation

--- app/src/main/java/org/tokend/template/features/signup/logic/SignUpUseCase.kt
+++ app/src/main/java/org/tokend/template/features/signup/logic/SignUpUseCase.kt
@@ -4,8 +4,11 @@ import io.reactivex.Single
 import io.reactivex.functions.BiFunction
 import org.tokend.rx.extensions.randomSingle
 import org.tokend.rx.extensions.toSingle
+import org.tokend.sdk.api.wallets.model.EmailAlreadyTakenException
+import org.tokend.sdk.api.wallets.model.InvalidCredentialsException
 import org.tokend.sdk.keyserver.KeyServer
 import org.tokend.sdk.keyserver.models.WalletCreateResult
+import org.tokend.template.util.confirmation.ConfirmationProvider
 import org.tokend.wallet.Account
 
 /**
@@ -14,19 +17,45 @@ import org.tokend.wallet.Account
 class SignUpUseCase(
         private val email: String,
         private val password: CharArray,
-        private val keyServer: KeyServer
+        private val keyServer: KeyServer,
+        private val recoverySeedConfirmation: ConfirmationProvider<CharArray>?
 ) {
     private lateinit var rootAccount: Account
     private lateinit var recoveryAccount: Account
 
     fun perform(): Single<WalletCreateResult> {
-        return getAccounts()
+        return ensureEmailIsFree()
+                .flatMap {
+                    getAccounts()
+                }
                 .doOnSuccess { (rootAccount, recoveryAccount) ->
                     this.rootAccount = rootAccount
                     this.recoveryAccount = recoveryAccount
                 }
                 .flatMap {
+                    confirmRecoverySeed()
+                }
+                .flatMap {
                     createAndSaveWallet()
+                }
+    }
+
+    private fun ensureEmailIsFree(): Single<Boolean> {
+        return keyServer
+                .getLoginParams(email)
+                .toSingle()
+                .map { false }
+                .onErrorResumeNext { error ->
+                    if (error is InvalidCredentialsException)
+                        Single.just(true)
+                    else
+                        Single.error(error)
+                }
+                .flatMap { isFree ->
+                    if (!isFree)
+                        Single.error(EmailAlreadyTakenException())
+                    else
+                        Single.just(isFree)
                 }
     }
 
@@ -40,6 +69,15 @@ class SignUpUseCase(
 
     private fun generateNewAccount(): Single<Account> {
         return Account.randomSingle()
+    }
+
+    private fun confirmRecoverySeed(): Single<Boolean> {
+        return if (recoverySeedConfirmation == null)
+            Single.just(true)
+        else
+            recoverySeedConfirmation
+                    .requestConfirmation(recoveryAccount.secretSeed!!)
+                    .toSingleDefault(true)
     }
 
     private fun createAndSaveWallet(): Single<WalletCreateResult> {
--- app/src/test/java/org/tokend/template/test/SignUpTest.kt
+++ app/src/test/java/org/tokend/template/test/SignUpTest.kt
@@ -2,19 +2,20 @@ package org.tokend.template.test
 
 import org.junit.Assert
 import org.junit.Test
+import org.tokend.sdk.api.wallets.model.EmailAlreadyTakenException
 import org.tokend.template.di.providers.ApiProviderFactory
 import org.tokend.template.features.signup.logic.SignUpUseCase
 
 class SignUpTest {
     @Test
-    fun signUp() {
+    fun aSignUp() {
         val urlConfigProvider = Util.getUrlConfigProvider()
 
         val email = Util.getEmail()
         val password = Config.DEFAULT_PASSWORD
         val keyStorage = ApiProviderFactory().createApiProvider(urlConfigProvider).getKeyServer()
 
-        val useCase = SignUpUseCase(email, password, keyStorage)
+        val useCase = SignUpUseCase(email, password, keyStorage, null)
 
         useCase.perform().blockingGet()
 
@@ -27,6 +28,38 @@ class SignUpTest {
         } catch (e: Exception) {
             Assert.fail("Wallet must be accessible with specified credentials")
         }
+    }
 
+    @Test
+    fun bSignUpWithExistingEmail() {
+        val urlConfigProvider = Util.getUrlConfigProvider()
+
+        val email = Util.getEmail()
+        val password = Config.DEFAULT_PASSWORD
+        val keyStorage = ApiProviderFactory().createApiProvider(urlConfigProvider).getKeyServer()
+
+        val useCase = SignUpUseCase(email, password, keyStorage, null)
+
+        try {
+            useCase.perform().blockingGet()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        try {
+            useCase.perform().blockingGet()
+        } catch (e: Exception) {
+            if (e is RuntimeException && e.cause is EmailAlreadyTakenException) {
+                // Nice.
+            } else {
+                e.printStackTrace()
+                Assert.fail("${EmailAlreadyTakenException::class.java.name} expected " +
+                        "but ${e::class.java.name} occurred")
+            }
+
+            return
+        }
+
+        Assert.fail("${EmailAlreadyTakenException::class.java.name} expected")
     }
 }
