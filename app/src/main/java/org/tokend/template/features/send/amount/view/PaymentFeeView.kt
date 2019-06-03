887400b378e29eaab9ee78ce4f7afccc3467c633
Oleg Koretsky <test.radiokot@gmail.com>
2019-06-03 12:33:27 +0300

    Created view for payment fee

--- app/src/main/java/org/tokend/template/features/send/amount/view/PaymentFeeView.kt
+++ app/src/main/java/org/tokend/template/features/send/amount/view/PaymentFeeView.kt
@@ -1,8 +1,16 @@
 package org.tokend.template.features.send.amount.view
 
 import android.content.Context
+import android.support.v7.widget.SwitchCompat
 import android.util.AttributeSet
-import android.widget.LinearLayout
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.TextView
+import org.jetbrains.anko.layoutInflater
+import org.tokend.template.R
+import org.tokend.template.view.util.formatter.AmountFormatter
+import java.math.BigDecimal
 
 /**
  * View for sender and recipient fees display
@@ -14,8 +22,101 @@ constructor(
         context: Context,
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr) {
-    init {
+) : FrameLayout(context, attrs, defStyleAttr) {
+    private val feesLayout: ViewGroup
+    private val feeTextView: TextView
+    private val recipientFeeLayout: ViewGroup
+    private val payRecipientFeeSwitch: SwitchCompat
+    private val loadingProgress: View
 
+    private var recipientFee: BigDecimal = BigDecimal.ZERO
+    private var senderFeeTotal: BigDecimal = BigDecimal.ZERO
+    private var assetCode: String = ""
+    private var amountFormatter: AmountFormatter? = null
+
+    init {
+        context.layoutInflater.inflate(R.layout.layout_payment_fee, this, true)
+
+        feesLayout = findViewById(R.id.fees_layout)
+        feeTextView = findViewById(R.id.sender_fee_text_view)
+        recipientFeeLayout = findViewById(R.id.recipient_fee_layout)
+        payRecipientFeeSwitch = findViewById(R.id.pay_recipient_fee_switch)
+        loadingProgress = findViewById(R.id.fees_loading_progress)
+
+        feesLayout.visibility = View.GONE
+        loadingProgress.visibility = View.GONE
+
+        payRecipientFeeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            onPayRecipientFeeChanged?.invoke(isChecked)
+            displayData()
+        }
+    }
+
+    var isLoading: Boolean = false
+        set(value) {
+            field = value
+
+            if (value) {
+                feesLayout.visibility = View.INVISIBLE
+                loadingProgress.visibility = View.VISIBLE
+            } else {
+                feesLayout.visibility = View.VISIBLE
+                loadingProgress.visibility = View.GONE
+            }
+        }
+
+    val isPayingRecipientFee: Boolean
+        get() = payRecipientFeeSwitch.isChecked
+
+    var onPayRecipientFeeChanged: ((Boolean) -> Unit)? = null
+
+    fun setFees(sender: BigDecimal,
+                recipient: BigDecimal,
+                assetCode: String,
+                amountFormatter: AmountFormatter) {
+        this.senderFeeTotal = if (!isPayingRecipientFee) sender else sender + recipient
+        this.recipientFee = recipient
+        this.assetCode = assetCode
+        this.amountFormatter = amountFormatter
+
+        displayData()
+    }
+
+    private fun displayData() {
+        val amountFormatter = this.amountFormatter
+                ?: return
+
+        if (senderFeeTotal.signum() == 0 && recipientFee.signum() == 0) {
+            feeTextView.text = context.getString(R.string.no_fee)
+            recipientFeeLayout.visibility = View.GONE
+            return
+        } else if (senderFeeTotal.signum() == 0 && recipientFee.signum() > 0) {
+            feeTextView.text = context.getString(
+                    R.string.template_recipients_fee,
+                    amountFormatter.formatAssetAmount(
+                            recipientFee,
+                            assetCode
+                    )
+            )
+            recipientFeeLayout.visibility = View.VISIBLE
+        } else if (senderFeeTotal.signum() > 0 && recipientFee.signum() == 0) {
+            feeTextView.text = context.getString(
+                    R.string.template_fee,
+                    amountFormatter.formatAssetAmount(
+                            senderFeeTotal,
+                            assetCode
+                    )
+            )
+            recipientFeeLayout.visibility = View.GONE
+        } else if (senderFeeTotal.signum() > 0 && recipientFee.signum() > 0) {
+            feeTextView.text = context.getString(
+                    R.string.template_fee,
+                    amountFormatter.formatAssetAmount(
+                            senderFeeTotal,
+                            assetCode
+                    )
+            )
+            recipientFeeLayout.visibility = View.VISIBLE
+        }
     }
 }
