e898ab7676fb6dc35bbbed7f14e8612421e99176
Oleg Koretsky <test.radiokot@gmail.com>
2019-07-02 18:11:29 +0300

    Use v3 transaction submit endpoint

--- app/src/main/java/org/tokend/template/logic/transactions/TxManager.kt
+++ app/src/main/java/org/tokend/template/logic/transactions/TxManager.kt
@@ -3,13 +3,12 @@ package org.tokend.template.logic.transactions
 import io.reactivex.Completable
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
+import org.tokend.rx.extensions.toSingle
 import org.tokend.sdk.api.transactions.model.SubmitTransactionResponse
 import org.tokend.template.di.providers.ApiProvider
-import org.tokend.rx.extensions.toSingle
 import org.tokend.template.util.confirmation.ConfirmationProvider
 import org.tokend.wallet.*
 import org.tokend.wallet.xdr.Operation
-import java.util.concurrent.TimeUnit
 
 /**
  * Manages transactions sending
@@ -26,13 +25,11 @@ class TxManager(
         return confirmationCompletable
                 .andThen(
                         apiProvider.getApi()
+                                .v3
                                 .transactions
-                                .submit(transaction.getEnvelope().toBase64())
+                                .submit(transaction, true)
                                 .toSingle()
                 )
-                // Magic delay is required because
-                // API doesn't sync with Horizon immediately.
-                .delay(1, TimeUnit.SECONDS)
     }
 
     companion object {
