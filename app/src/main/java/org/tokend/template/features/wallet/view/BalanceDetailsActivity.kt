905501a0c08c5d4940979f810bb33d8ec30db4f6
Oleg Koretsky <test.radiokot@gmail.com>
2019-12-04 13:41:42 +0200

    Fixed invalid asset withdrawability check on balance details screen

--- app/src/main/java/org/tokend/template/features/wallet/view/BalanceDetailsActivity.kt
+++ app/src/main/java/org/tokend/template/features/wallet/view/BalanceDetailsActivity.kt
@@ -199,7 +199,7 @@ class BalanceDetailsActivity : BaseActivity() {
                         val assetCode = asset?.code ?: return@FloatingActionMenuAction
                         navigator.openWithdraw(0, assetCode)
                     },
-                    isEnabled = asset?.isBackedByExternalSystem == true
+                    isEnabled = asset?.isWithdrawable == true
             ))
         }
 
