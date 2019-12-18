9bfd000e19251fc409156a049f0c3cea0bec3f66
Oleg Koretsky <test.radiokot@gmail.com>
2019-12-18 12:22:35 +0200

    Removed redundant stack trace and added method and URL to HTTP exception logging

--- app/src/main/java/org/tokend/template/util/errorhandler/DefaultErrorLogger.kt
+++ app/src/main/java/org/tokend/template/util/errorhandler/DefaultErrorLogger.kt
@@ -2,13 +2,40 @@ package org.tokend.template.util.errorhandler
 
 import com.crashlytics.android.Crashlytics
 import org.tokend.template.BuildConfig
+import retrofit2.HttpException
+import retrofit2.Response
 
 class DefaultErrorLogger : ErrorLogger {
+    private class WrappedHttpException(
+            private val httpException: HttpException
+    ) : Exception(httpException) {
+        override val message: String?
+            get() {
+                val request = httpException.response()?.raw()?.request()
+                        ?: return httpException.message
+                return httpException.message() + " (${request.method()} ${request.url()})"
+            }
+    }
 
     override fun log(error: Throwable) {
-        if (BuildConfig.ENABLE_ANALYTICS) {
-            Crashlytics.logException(error)
+        var e = error
+        
+        if (error is HttpException) {
+            val rawResponse = error.response().raw()
+            val request = rawResponse.request()
+            e = HttpException(Response.error<Any>(
+                    error.response().errorBody(),
+                    rawResponse
+                            .newBuilder()
+                            .message(rawResponse.message() + " (${request.method()} ${request.url()})")
+                            .build()
+            ))
+            e.stackTrace = emptyArray()
         }
-        error.printStackTrace()
+
+        if (BuildConfig.ENABLE_ANALYTICS) {
+            Crashlytics.logException(e)
+        }
+        e.printStackTrace()
     }
 }
