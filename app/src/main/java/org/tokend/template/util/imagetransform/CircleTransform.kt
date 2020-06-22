d074689cb770d1ba5ed2bc8cebb131a4b56e640b
Oleg Koretsky <test.radiokot@gmail.com>
2020-06-22 17:43:38 +0300

    Fix Picasso circle transform to avoid usage of recycled bitmaps

--- app/src/main/java/org/tokend/template/util/imagetransform/CircleTransform.kt
+++ app/src/main/java/org/tokend/template/util/imagetransform/CircleTransform.kt
@@ -1,36 +1,52 @@
 package org.tokend.template.util.imagetransform
 
+/**
+ * Copyright (C) 2018 Wasabeef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import android.graphics.*
 import com.squareup.picasso.Transformation
+import kotlin.math.min
 
-/**
- * Bitmap transformation making it circled
- */
 class CircleTransform : Transformation {
 
     override fun transform(source: Bitmap): Bitmap {
-        val size = Math.min(source.width, source.height)
+        val size = min(source.width, source.height)
 
-        val x = (source.width - size) / 2
-        val y = (source.height - size) / 2
-
-        val squaredBitmap = Bitmap.createBitmap(source, x, y, size, size)
-        if (squaredBitmap != source) {
-            source.recycle()
-        }
+        val width = (source.width - size) / 2
+        val height = (source.height - size) / 2
 
         val bitmap = Bitmap.createBitmap(size, size, source.config)
 
         val canvas = Canvas(bitmap)
         val paint = Paint()
-        val shader = BitmapShader(squaredBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+        val shader = BitmapShader(source, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+        if (width != 0 || height != 0) {
+            // source isn't square, move viewport to center
+            val matrix = Matrix()
+            matrix.setTranslate(-width.toFloat(), -height.toFloat())
+            shader.setLocalMatrix(matrix)
+        }
         paint.shader = shader
         paint.isAntiAlias = true
 
         val r = size / 2f
         canvas.drawCircle(r, r, r, paint)
 
-        squaredBitmap.recycle()
+        source.recycle()
+
         return bitmap
     }
 
